02fbcfaf2338b0505302c62df4f09d256f3a206a
Tthiagoo <51219408+Tthiagoo@users.noreply.github.com>
2021-06-07 09:36:08 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,61 +1,64 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import { ChallengeBox } from '../components/ChallengeBox';
-import { ExperienceBar } from '../components/ExperienceBar';
-import { Profile } from '../components/Profile';
-import { Countdown } from '../components/Countdown';
-import { CompletedChallenges } from '../components/CompletedChallenges';
+import { ChallengeBox } from "../components/ChallengeBox";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { Countdown } from "../components/Countdown";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { ChallengesProvider } from "../contexts/ChallengesContext";
+import { CountdownProvider } from "../contexts/CountdownContext";
 
-import { ChallengesProvider } from '../contexts/ChallengesContext';
-import { CountdownProvider } from '../contexts/CountdownContext';
-
-import styles from '../styles/pages/Home.module.css'
-import { GetServerSideProps } from 'next';
+import styles from "../styles/pages/Home.module.css";
+import { GetServerSideProps } from "next";
 
 interface HomeProps {
-  level: number;
-  currentExperience: number;
-  challengesCompleted: number;
+	level: number;
+	currentExperience: number;
+	challengesCompleted: number;
 }
 
-export default function Home({ level, currentExperience, challengesCompleted}: HomeProps) {
-  return (
-    <ChallengesProvider
-      level={level} 
-      currentExperience={currentExperience} 
-      challengesCompleted={challengesCompleted}
-    >
-      <CountdownProvider>
-        <main className={styles.container}>
-          <Head>
-            <title>move.it | Início</title>
-          </Head> 
+export default function Home({
+	level,
+	currentExperience,
+	challengesCompleted,
+}: HomeProps) {
+	return (
+		<ChallengesProvider
+			level={level}
+			currentExperience={currentExperience}
+			challengesCompleted={challengesCompleted}
+		>
+			<CountdownProvider>
+				<main className={styles.container}>
+					<Head>
+						<title>move.it | Início</title>
+					</Head>
 
-          <ExperienceBar />
+					<ExperienceBar />
 
-          <section>
-            <div className={styles.cycleContainer}>
-              <Profile />
-              <CompletedChallenges />
-              <Countdown />
-            </div>
+					<section>
+						<div className={styles.cycleContainer}>
+							<Profile />
+							<CompletedChallenges />
+							<Countdown />
+						</div>
 
-            <ChallengeBox />
-          </section>
-        </main>
-      </CountdownProvider>
-    </ChallengesProvider>
-  )
+						<ChallengeBox />
+					</section>
+				</main>
+			</CountdownProvider>
+		</ChallengesProvider>
+	);
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  const { level, currentExperience, challengesCompleted } = req.cookies;
+	const { level, currentExperience, challengesCompleted } = req.cookies;
 
-  return {
-    props: {
-      level: Number(level),
-      currentExperience: Number(currentExperience),
-      challengesCompleted: Number(challengesCompleted),
-    }
-  }
+	return {
+		props: {
+			level: Number(level),
+			currentExperience: Number(currentExperience),
+			challengesCompleted: Number(challengesCompleted),
+		},
+	};
 };
